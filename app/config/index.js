62049a4cc79b16f19893fd2bd40a04eb1afb5470
Yogesh H <you@example.com>
2017-04-05 22:35:30 +0530

    Validations Added

--- app/config/index.js
+++ app/config/index.js
@@ -2,7 +2,7 @@ var credentials = require('./credentials');
 
 module.exports = {
 	dbConnection: function() {
-		// return "mongodb://"+ credentials.username +":"+ credentials.password +"@ds131340.mlab.com:31340/firstappdb";
-		return "mongodb://localhost/firstappdb";
+		return "mongodb://"+ credentials.username +":"+ credentials.password +"@ds131340.mlab.com:31340/firstappdb";
+		// return "mongodb://localhost/firstappdb";
 	}
 }
